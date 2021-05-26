4d205d7f74f8396a03703233756594876cfbf47a
sairammanem <sairammanem@gmail.com>
2021-05-26 15:46:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Shorturl = require('./models/shorturls')
+
+const app = express()
+let dburl  = "mongodb+srv://sairam:sairamvirat@cluster0.qxjo5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+mongoose.connect(dburl,{
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', async (req,res)=> {
+    const shorturls = await Shorturl.find()
+    res.render('index',{shorturls:shorturls})
+})
+
+
+app.post('/shorturls',async (req,res)=>{
+  await Shorturl.create({full: req.body.fullurl})
+  res.redirect('/')
+})
+
+app.get('/:shorturl', async (req,res)=> {
+
+  const shorturl = await Shorturl.findOne({short: req.params.shorturl})
+ if(shorturl == null) return res.sendStatus(404)
+
+ shorturl.clicks++
+ shorturl.save()
+
+ res.redirect(shorturl.full)
+})
+
+
+app.listen(process.env.PORT || 5000)
